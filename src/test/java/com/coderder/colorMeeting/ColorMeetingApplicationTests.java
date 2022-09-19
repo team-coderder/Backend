751eac7597ecb7e459029477a3a78ce8ff2a51a9
60067550 <justin403502@naver.com>
2022-09-19 20:55:54 +0900

    create spring project

---
+++ src/test/java/com/coderder/colorMeeting/ColorMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderder.colorMeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
