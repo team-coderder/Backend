0afa69d0b3e0a6b85ea06311fc44a463c1a3880c
강정구 <57992058+JungguKang@users.noreply.github.com>
2022-09-27 21:22:53 +0900

    Squashed commit of the following:
    commit e32c835a6e80e5d567a097f314e557c4083f5ce7
    Author: 강정구 <57992058+JungguKang@users.noreply.github.com>
    Date:   Fri Sep 23 21:03:13 2022 +0900
    
        AppSpec -> appspec
    
    commit 4a5764d718c1eff8ab3a7f19e3880a798b870542
    Author: 강정구 <57992058+JungguKang@users.noreply.github.com>
    Date:   Fri Sep 23 21:00:11 2022 +0900
    
        add ci-cd appspec file
    
    commit d8af97af219f49902f5de8f69cb5b75f23c0a6fd
    Merge: 342e693 faec385
    Author: 강정구 <57992058+JungguKang@users.noreply.github.com>
    Date:   Thu Sep 22 20:23:39 2022 +0900
    
        Merge pull request #1 from team-coderder/deploy-first-settings
    
        Create deploy.yml
    
    commit faec38598ce9dddf86ece2344df40b249db17e96
    Merge: 18314a5 342e693
    Author: 60067550 <justin403502@naver.com>
    Date:   Thu Sep 22 20:21:14 2022 +0900
    
        Merge branch 'main' into deploy-first-settings
    
    commit 342e6934009f737adfeb07d2a37b23b3cfb9c5db
    Author: 60067550 <justin403502@naver.com>
    Date:   Thu Sep 22 19:29:20 2022 +0900
    
        add-gitignore
    
    commit 18314a513e4bb8cefbe832c3c08866ac61c27a2d
    Author: 강정구 <57992058+JungguKang@users.noreply.github.com>
    Date:   Tue Sep 20 20:54:36 2022 +0900
    
        Create deploy.yml
    
    commit 751eac7597ecb7e459029477a3a78ce8ff2a51a9
    Author: 60067550 <justin403502@naver.com>
    Date:   Mon Sep 19 20:55:54 2022 +0900
    
        create spring project

---
+++ src/main/java/com/coderder/colorMeeting/ColorMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.coderder.colorMeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorMeetingApplication.class, args);
+	}
+
+}
