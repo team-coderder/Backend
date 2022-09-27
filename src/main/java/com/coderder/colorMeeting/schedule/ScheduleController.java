5e591ae6bb234aee3481690047e612f5bb3344de
60067550 <justin403502@naver.com>
2022-09-27 17:09:48 +0900

    feat - 초기 뼈대 작업

---
+++ src/main/java/com/coderder/colorMeeting/schedule/ScheduleController.java
@@ -0,0 +1,13 @@
+package com.coderder.colorMeeting.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ScheduleController {
+
+    @Autowired
+    private ScheduleRepository;
+
+    
+}
---
+++ src/test/java/com/coderder/colorMeeting/schedule/ScheduleServiceTest.java
@@ -0,0 +1,16 @@
+package com.coderder.colorMeeting.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ScheduleServiceTest {
+
+    @Autowired
+    ScheduleRepository scheduleRepository;
+
+    //일단 일정 가져오는 서비스를 만들어야 함
+    @Test
+    void getScheduleTest() {
+
+    }
+}
