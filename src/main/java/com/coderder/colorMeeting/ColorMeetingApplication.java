5a7357e251a7621f9536f4467db7dab642e2e60e
진지연 <serious_yeon@naver.com>
2022-10-01 15:41:19 +0900

    feat: spring과 h2 연동하기

--- src/main/java/com/coderder/colorMeeting/ColorMeetingApplication.java
+++ src/main/java/com/coderder/colorMeeting/ColorMeetingApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-//@SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ColorMeetingApplication {
 
 	public static void main(String[] args) {
