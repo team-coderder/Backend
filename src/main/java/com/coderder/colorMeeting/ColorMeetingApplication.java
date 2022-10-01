b40d75024e79df36154b2f27a8c23353839f736d
진지연 <serious_yeon@naver.com>
2022-10-01 14:54:40 +0900

    feat: db 연동 없이 빌드하기

--- src/main/java/com/coderder/colorMeeting/ColorMeetingApplication.java
+++ src/main/java/com/coderder/colorMeeting/ColorMeetingApplication.java
@@ -2,8 +2,10 @@ package com.coderder.colorMeeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+//@SpringBootApplication
 public class ColorMeetingApplication {
 
 	public static void main(String[] args) {
