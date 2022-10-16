93c6ffd6c10e1c19824fb0e817309c0ff6332459
진지연 <serious_yeon@naver.com>
2022-10-16 13:53:10 +0900

    #23 feat : jpa repository 로 변경

--- src/main/java/com/coderder/colorMeeting/repository/MemberRepository.java
+++ src/main/java/com/coderder/colorMeeting/repository/MemberRepository.java
@@ -1,23 +1,8 @@
 package com.coderder.colorMeeting.repository;
 
 import com.coderder.colorMeeting.model.Member;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@Repository
-public class MemberRepository {
-
-    @PersistenceContext
-    EntityManager em;
-
-    public Long save(Member member) {
-        em.persist(member);
-        return member.getId();
-    }
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
