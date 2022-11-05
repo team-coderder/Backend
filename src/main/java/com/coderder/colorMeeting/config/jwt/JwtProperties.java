78c7f7c80360ba102863cab0ecf1e0509fd57b4b
진지연 <serious_yeon@naver.com>
2022-11-05 13:32:35 +0900

    # fix : 환경변수에서 시크릿값 읽어오기

--- src/main/java/com/coderder/colorMeeting/config/jwt/JwtProperties.java
+++ src/main/java/com/coderder/colorMeeting/config/jwt/JwtProperties.java
@@ -6,18 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtProperties {
     @Value("${jwt.secret.key}")
-    private static String TEST;
+    private String TEST;
 //    private static String SECRET; // 우리 서버만 알고 있는 비밀값
-    static String SECRET = "colorMeeting";
+//    @Value("${jwt.secret.key}")
+//    static String SECRET;
     static int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
     static String TOKEN_PREFIX = "Bearer ";
     static String HEADER_STRING = "Authorization";
 
-    public static String getSECRET() {
-        return SECRET;
-    }
+//    public static String getSECRET() {
+//        return SECRET;
+//    }
 
-    public static String getTEST() {
+    public String getTEST() {
         return TEST;
     }
 }
