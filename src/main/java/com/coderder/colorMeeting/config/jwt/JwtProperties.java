12597b661828c6bf8e3233f864ecd9be5e3b4d5c
진지연 <serious_yeon@naver.com>
2022-11-05 13:38:37 +0900

    #23 refactor : 코드 정리

--- src/main/java/com/coderder/colorMeeting/config/jwt/JwtProperties.java
+++ src/main/java/com/coderder/colorMeeting/config/jwt/JwtProperties.java
@@ -5,20 +5,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProperties {
+    // @Value("${jwt.secret.key}")
+    // private String TEST;
+    // try 1 : 깃에 노출됨
+    // private static String SECRET = "secret";
+    // try 2 : 읽어오기
     @Value("${jwt.secret.key}")
-    private String TEST;
-//    private static String SECRET; // 우리 서버만 알고 있는 비밀값
-//    @Value("${jwt.secret.key}")
-//    static String SECRET;
+    private String SECRET;
     static int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
     static String TOKEN_PREFIX = "Bearer ";
     static String HEADER_STRING = "Authorization";
 
-//    public static String getSECRET() {
-//        return SECRET;
-//    }
-
-    public String getTEST() {
-        return TEST;
+    public String getSECRET() {
+        return SECRET;
     }
+
+    // public String getTEST() {
+    //     return TEST;
+    // }
 }
