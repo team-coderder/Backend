ef84868c42693b0b721529bf66b0665723580fea
송민진 <m_6595@naver.com>
2023-01-20 22:55:28 +0900

    #45 feat(jwtUtil) : 디버깅용 로깅 코드 작성

--- src/main/java/com/coderder/colorMeeting/config/jwt/JwtUtil.java
+++ src/main/java/com/coderder/colorMeeting/config/jwt/JwtUtil.java
@@ -5,16 +5,22 @@ import com.coderder.colorMeeting.exception.ErrorCode;
 import com.coderder.colorMeeting.exception.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtUtil {
     private final ObjectMapper objectMapper;
+
+    private final Logger logger = LoggerFactory.getLogger("JwtUtil 의 로그");
 
     // 예외 응답
     public void exceptionResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
@@ -22,8 +28,23 @@ public class JwtUtil {
         response.setCharacterEncoding("UTF-8");
 //        ResponseDto<?> responseDto = ResponseDto.fail(errorCode);
         ErrorResponse errorResponse = new ErrorResponse(errorCode);
+//        String httpResponse = objectMapper.writeValueAsString(errorResponse);
         String httpResponse = objectMapper.writeValueAsString(errorResponse);
+
+        logger.debug("response에 Write 전");
+        logger.debug(httpResponse);
+        logger.debug(httpResponse.toString());
+
+//        log.debug("어노테이션, response에 Write 전");
+//        log.debug(httpResponse);
+
         response.getWriter().write(httpResponse);
+
+        logger.debug("response에 Write 후");
+        logger.debug(response.toString());
+
+//        log.debug("어노테이션, response에 Write 후");
+//        log.debug(response.toString());
     }
 
 }
