1c5641b7899c096973fd9aacdd66e32bfef75c90
kangjunggu <justin403502@naver.com>
2023-01-28 20:12:00 +0900

    feat : json 하이픈 제거

--- src/main/java/com/coderder/colorMeeting/dto/request/ScheduleRequestDto.java
+++ src/main/java/com/coderder/colorMeeting/dto/request/ScheduleRequestDto.java
@@ -4,21 +4,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalTime;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class ScheduleRequestDto {
     private Long userId;
     private String name;
     private String weekday;
-    @JsonProperty("start-time")
+    @JsonProperty("start")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime startTime;
 
-    @JsonProperty("finish-time")
+    @JsonProperty("finish")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime finishTime;
     private String memo;
--- src/main/java/com/coderder/colorMeeting/dto/request/TeamScheduleRequestDto.java
+++ src/main/java/com/coderder/colorMeeting/dto/request/TeamScheduleRequestDto.java
@@ -1,26 +1,23 @@
 package com.coderder.colorMeeting.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalTime;
 
 
 @Data
-@Builder
+@NoArgsConstructor
 public class TeamScheduleRequestDto {
     private Long teamId;
     private String name;
     private String weekday;
-    @JsonProperty("start-time")
+    @JsonProperty("start")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime startTime;
 
-    @JsonProperty("finish-time")
+    @JsonProperty("finish")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime finishTime;
     private String memo;
